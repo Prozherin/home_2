82dc5085a0dc73c1bdb6bab4c56216e96fd0a535
Prozherin <taskacik@gmail.com>
2023-09-07 21:47:42 +0500

    Домашняя работа 5

---
+++ task_36/Program.cs
@@ -0,0 +1,20 @@
+﻿// // Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// // [3, 7, 23, 12]-> 19
+// // [-4, -6, 89, 6]-> 0
+
+
+List<int> numbers = new List<int>();
+numbers.Add(3);
+numbers.Add(7);
+numbers.Add(23);
+numbers.Add(12);
+
+int sumOfOddElements = 0;
+
+for (int i = 1; i <= numbers.Count - 1; i += 2)
+{
+    sumOfOddElements += numbers[i];
+}
+
+Console.Write("Сумма элементов на нечетных позициях: ");
+Console.WriteLine(sumOfOddElements);
---
+++ task_38/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+float[] array = new float[5];
+Random random = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    // double array = random.NextDouble() * 99;
+    array[i] = random.NextSingle() * 99;
+}
+
+float maxValue = array.Max();
+float minValue = array.Min();
+
+double difference = maxValue - minValue;
+
+Console.WriteLine(String.Join(" ", array));
+Console.WriteLine($" {array.Max()} - {array.Min()} = " + difference);
